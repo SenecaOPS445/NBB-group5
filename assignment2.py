d391511e44e6f54ba7fae420c53d7abe48368409
Dinesh Nepal <dnepal@myseneca.ca>
2024-11-23 13:25:44 -0500

    assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -9,4 +9,11 @@ import tarfile
 def data_backup(source_dir, backup_dir, archive_name):
     ''' This function backups the data from the provided filepath, compress it into .tar.gz and save it into the provided filepath backup directory'''
 
+   if not os.path.isdir(source_dir): # checks if the source directory exists, if doesn't exists it gives a error message"
+        print(f"Error: Source directory '{source_dir}' not found.")
+        return None
     
+    os.makedirs(backup_dir, exist_ok=True) # Checks if backup directory exists, if it doesn't exist it creates a new directory.
+
+    archive_path = os.path.join(backup_dir, f"{archive_name}.tar.gz") # Full path for the archive
+ 
